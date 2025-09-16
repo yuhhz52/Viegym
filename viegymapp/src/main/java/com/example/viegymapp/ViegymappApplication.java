50153f5224102f485081f6ea67f0c2a9d3728d8a
Huy Nguyen <thanhyuh0910@gmail.com>
2025-09-16 19:32:23 +0700

    Initial commit: add frontend, backend, docker setup

---
+++ viegymapp/src/main/java/com/example/viegymapp/ViegymappApplication.java
@@ -0,0 +1,13 @@
+package com.example.viegymapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViegymappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViegymappApplication.class, args);
+	}
+
+}
