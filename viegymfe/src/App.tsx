b7685acbb990b0db84aec3f8c7a23653352a121f
Huy Nguyen <thanhyuh0910@gmail.com>
2025-09-18 16:47:57 +0700

    tsx

---
+++ viegymfe/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold underline text-red-600">
+        Hello Tailwind + React + Vite 
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
